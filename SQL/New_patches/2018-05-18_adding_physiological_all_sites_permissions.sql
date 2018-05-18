e4301dbf5158aa38ea088c063c3576362ae427be
Cecile Madjar <cecile.madjar@gmail.com>
2018-05-18 15:55:11 -0400

    adding SQL patch for permissions to physiological module

---
+++ SQL/New_patches/2018-05-18_adding_physiological_all_sites_permissions.sql
@@ -0,0 +1,12 @@
+INSERT INTO permissions 
+    (
+        code, 
+        description, 
+        type, 
+        categoryID
+    ) VALUES (
+        'physiological_browser_view_allsites', 
+        'View all-sites Imaging Browser pages',
+        'permission',
+        3
+    );
