3181dd7572b55fb81b92a5610dfc8369e2587be4
Cecile Madjar <cecile.madjar@gmail.com>
2018-05-24 18:18:40 -0400

    added permission checks on which data can be viewed in the datatable

--- SQL/New_patches/2018-05-18_adding_physiological_all_sites_permissions.sql
+++ SQL/New_patches/2018-05-18_adding_physiological_all_sites_permissions.sql
@@ -5,8 +5,21 @@ INSERT INTO permissions
         type, 
         categoryID
     ) VALUES (
-        'physiological_browser_view_allsites', 
-        'View all-sites Imaging Browser pages',
+        'electrophysiology_browser_view_allsites',
+        'View all-sites Electrophysiology Browser pages',
         'permission',
-        3
+        (SELECT ID FROM permissions_category WHERE Description='Permission')
+    );
+
+INSERT INTO permissions
+    (
+        code,
+        description,
+        type,
+        categoryID
+    ) VALUES (
+        'electrophysiology_browser_view_site',
+        'View own site Electrophysiology Browser pages',
+        'permission',
+        (SELECT ID FROM permissions_category WHERE Description='Permission')
     );
